e54b51a4b1ec948731305c2b4082fab2a97b4b9d
Edmond Burnett <_@edmondburnett.com>
2026-01-08 16:09:05 -0800

    init cargo project

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use axum::{Router, routing::get};
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new().route("/", get(root)).layer(TraceLayer::new_for_http());
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "edmondburnett.com"
+}
