4234f554419f9f7a0236d8ec96498ad6cfb3c009
Edmond Burnett <_@edmondburnett.com>
2026-06-04 16:48:40 -0700

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ cargo build --release
 sudo systemctl stop edmondburnett-com.service
 #sudo mkdir -p /srv/http/edmondburnett.com
 #sudo useradd --system --no-create-home --shell /usr/bin/nologin edmondburnett-com
+sudo cp conf/systemd/edmondburnett-com.service /srv/http/edmondburnett.com
 sudo cp target/release/edmondburnett-com /srv/http/edmondburnett.com
 sudo cp -r pages /srv/http/edmondburnett.com
 sudo cp -r posts /srv/http/edmondburnett.com
