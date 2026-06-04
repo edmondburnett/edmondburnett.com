ace202c7ae4fed329ce535f6f848bb207c7ccc6d
Edmond Burnett <_@edmondburnett.com>
2026-06-03 22:54:42 -0700

    improve initial deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
 cargo build --release
+sudo systemctl stop edmondburnett-com.service
 #sudo mkdir -p /srv/http/edmondburnett.com
 #sudo useradd --system --no-create-home --shell /usr/bin/nologin edmondburnett-com
 sudo cp target/release/edmondburnett-com /srv/http/edmondburnett.com
@@ -9,6 +10,6 @@ sudo cp -r posts /srv/http/edmondburnett.com
 sudo cp -r projects /srv/http/edmondburnett.com
 sudo cp -r static /srv/http/edmondburnett.com
 sudo chown -R edmondburnett-com:edmondburnett-com /srv/http/edmondburnett.com
-#sudo systemctl link /srv/http/edmondburnett.com/edmondburnett-com.service
+sudo systemctl link /srv/http/edmondburnett.com/edmondburnett-com.service
 sudo systemctl daemon-reload
 sudo systemctl restart edmondburnett-com.service
