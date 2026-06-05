575dcf5ba4388933647a45c475e469952f4f0164
Edmond Burnett <_@edmondburnett.com>
2026-06-04 17:11:41 -0700

    remove existing data on deploy

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@ cargo build --release
 sudo systemctl stop edmondburnett-com.service
 #sudo mkdir -p /srv/http/edmondburnett.com
 #sudo useradd --system --no-create-home --shell /usr/bin/nologin edmondburnett-com
+sudo rm /srv/http/edmondburnett.com/posts/*
+sudo rm /srv/http/edmondburnett.com/pages/*
+sudo rm /srv/http/edmondburnett.com/projects/*
+sudo rm /srv/http/edmondburnett.com/static/*
 sudo cp conf/systemd/edmondburnett-com.service /srv/http/edmondburnett.com
 sudo cp target/release/edmondburnett-com /srv/http/edmondburnett.com
 sudo cp -r pages /srv/http/edmondburnett.com
--- posts/meta-ai-crawling-ddos.md
+++ posts/meta-ai-crawling-ddos.md
@@ -4,7 +4,7 @@ title: DDoS'd by Meta
 description: Handling a denial-of-service incident caused by Meta AI crawlers.
 date: 2026-05-21T12:46:17Z
 updated: 2026-05-21T12:46:19Z
-draft: false
+draft: true
 tags:
     - meta
     - facebook
